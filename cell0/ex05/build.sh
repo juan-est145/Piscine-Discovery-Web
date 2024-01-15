3eb2425d2be8ef5c3d92bddb4913ccd2cb656709
Juan Jose Estrella Ramirez <128648875+juan-est145@users.noreply.github.com>
2024-01-15 17:03:24 +0100

    Final script done

--- cell0/ex05/build.sh
+++ cell0/ex05/build.sh
@@ -3,10 +3,8 @@
 if [ "$#" -eq 0 ]; then
     echo "No arguments supplied"
 else
-iterator=0
-while [ $iterator -le "$#" ]
-do
-    mkdir ex$i
-    ((i++))
-done
+for i in $@
+    do
+        mkdir "ex$i"
+    done
 fi
