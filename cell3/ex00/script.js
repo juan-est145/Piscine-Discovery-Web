cc1e1e5d654ee847704fa17a60abf583ea6b2708
Juan Jose Estrella Ramirez <128648875+juan-est145@users.noreply.github.com>
2024-01-18 18:29:46 +0100

    Button works

---
+++ cell3/ex00/script.js
@@ -0,0 +1,19 @@
+let Botón = document.getElementById("Botón");
+
+
+function create_random_color()
+{
+    let arrayOfColors = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+
+    let color_string = "";
+
+    for(let i = 0; i < 6; i++)
+    {
+        color_string += arrayOfColors[Math.floor(Math.random()* 15)];
+    }
+    return `#${color_string}`;
+}
+
+Botón.addEventListener('click', () => {
+    document.body.style.backgroundColor = create_random_color();
+  });
